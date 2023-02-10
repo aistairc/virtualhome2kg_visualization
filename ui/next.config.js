5a8d1c73f1064c4a918bc8b93bd391c2de00b66e
sasau <fumiya.kubota@glucose.jp>
2023-02-10 13:48:53 +0900

    Update next.config.js

--- ui/next.config.js
+++ ui/next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.GHPAGES === 'true';
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  assetPrefix: isProd ? '/risk-ui' : '',
+  assetPrefix: isProd ? '/virtualhome2kg_visualization' : '',
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.fallback = { net: false, fs: false, child_process: false }
