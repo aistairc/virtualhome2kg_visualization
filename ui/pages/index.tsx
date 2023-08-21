9dafe6b11551b0bc564f412def417b86c9dca3fd
YE-WIN-Unity <107833034+YE-WIN-Unity@users.noreply.github.com>
2023-08-21 16:41:33 +0900

    Add 3D link

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -37,7 +37,7 @@ import { yellow } from "@mui/material/colors";
 import { ObjectTable } from "../components/ObjectTable";
 import dynamic from "next/dynamic";
 import { makeMoviePath } from "../utils/movieDir";
-import { htmlFile } from "../components/virtualhome2kg-admire_art1_scene1.html";
+import htmlFile from "../components/virtualhome2kg-admire_art1_scene1.html";
 
 type ActivityListType = {
   type: string;
@@ -216,11 +216,9 @@ const Home: NextPage = () => {
   const onClick3D = (async () => {
     if (activity){
       console.log(activity);
-      console.log(x3ds);
-      let htmlName = activity.activity.value.replace(PREFIXES.ex, "");
-      let htmlPath = '../../3D/virtualhome2kg-'+htmlName+'.html';
+      //let htmlName = activity.activity.value.replace(PREFIXES.ex, "");
+      //let htmlPath = '../../3D/virtualhome2kg-'+htmlName+'.html';
       //const htmlFile = await import(htmlPath);
-      //console.log(htmlFile);
       const htmlContent = await htmlFile.default.text();
       const newTab = window.open('', '_blank');
       newTab.document.write('<!DOCTYPE html><html><head><title>Dynamic HTML</title></head><body>');
