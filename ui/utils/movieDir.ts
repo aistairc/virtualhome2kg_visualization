3da4f8b18c5113d3761aeca6fd57231a8fec6883
YE-WIN-Unity <107833034+YE-WIN-Unity@users.noreply.github.com>
2023-08-22 16:18:05 +0900

    Add request Header

--- ui/utils/movieDir.ts
+++ ui/utils/movieDir.ts
@@ -21,7 +21,7 @@ export const makeMoviePath = async (
       PREFIXES.ho,
       ""
     )}/${sceneFilename}`;
-    console.log(url);
+    console.log(url+"<<<<<<<<<<<<<<");
     try {
       await axios.head(url, {
         headers: {
